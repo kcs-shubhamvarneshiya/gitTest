24459b46662748464751301e057c47683d460b71
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-03-30 15:52:03 +0530

    change semicolon msg

--- powerfull.js
+++ powerfull.js
@@ -1,3 +1,4 @@
 console.log('Hello world , jay shree Ram , jay shree Krishna !!')
 console.log('Hello world , jay shree Ram , jay shree Krishna !!')
 console.log('Hello world , jay shree Ram , jay shree Krishna !!')
+console.log('Hello semicolon');
